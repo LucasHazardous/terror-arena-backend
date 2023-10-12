e08c7c8ab8566742ed99e3920ab1068acf605b8f
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-10-12 17:21:59 +0200

    installed cors, use express instead of startStandaloneServer

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,25 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
 
+import express, { json } from "express";
+import cors from "cors";
+import { expressMiddleware } from "@apollo/server/express4";
+
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-const { url } = await startStandaloneServer(server, {
-    listen: {
-        port: 4000
-    }
-});
+await server.start();
 
-console.log(url);
+const app = express();
+app.use('/graphql', cors<cors.CorsRequest>(), json(), expressMiddleware(server));
+app.listen(4000);
+
+// const { url } = await startStandaloneServer(server, {
+//     listen: {
+//         port: 4000
+//     }
+// });
+
+// console.log(url);
